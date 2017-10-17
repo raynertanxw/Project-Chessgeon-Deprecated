44ef48cb75d629853d6eb36d03430b269f0fa444
Tan Xue Wen, Rayner <raynertanxw@me.com>
2017-10-17 17:11:47 +0800

    Created Async Loading Screen sequence.

---
+++ Chessgeon/Assets/_Game/Scripts/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants
+{
+	public const int SCENE_LOADINGSCREEN = 0;
+	public const int SCENE_MENU = 1;
+	public const int SCENE_DUNGEON = 2;
+	public const string SCENE_NAME_LOADINGSCREEN = "SCENE_LoadingScreen";
+	public const string SCENE_NAME_MENU = "SCENE_Menu";
+	public const string SCENE_NAME_DUNGEON = "SCENE_Dungeon";
+}
---
+++ Chessgeon/Assets/_Game/Scripts/LoadingScreen.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour
+{
+	[SerializeField] private AudioListener _loadingScreenAudioListener = null;
+	[SerializeField] private Transform _loadingIndicator = null;
+
+	void Awake()
+	{
+		Debug.Assert(_loadingScreenAudioListener != null, "_loadingScreenAudioListener in LoadingScreen.cs was not assigned.");
+		Debug.Assert(_loadingIndicator != null, "_loadingIndicator in LoadingScreen.cs was not assigned.");
+	}
+
+	void Start()
+	{
+		StartCoroutine(LoadMenuSceneAsync());
+	}
+
+	void Update()
+	{
+		// TODO: Just a temporary placeholder for loading indication.
+		_loadingIndicator.Rotate(Vector3.forward, -500.0f * Time.deltaTime);
+	}
+
+	IEnumerator LoadMenuSceneAsync()
+	{
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(Constants.SCENE_MENU, LoadSceneMode.Additive);
+		asyncLoad.allowSceneActivation = false; // Stops scene load at 0.9 and prevents it from being activated immediately when ready.
+
+		yield return new WaitForSeconds(5.0f); // TODO: Artificial wait loading time.
+		while (!asyncLoad.isDone)
+		{
+			if (asyncLoad.progress >= 0.9f) // If ready to be activated.
+			{
+				_loadingScreenAudioListener.enabled = false;
+				asyncLoad.allowSceneActivation = true;
+			}
+			yield return null;
+		}
+
+		Scene menuScene = SceneManager.GetSceneByName(Constants.SCENE_NAME_MENU);
+		SceneManager.MoveGameObjectToScene(this.gameObject, menuScene);
+		StartCoroutine(UnloadLoadingScreenSceneAsync());
+	}
+
+	IEnumerator UnloadLoadingScreenSceneAsync()
+	{
+		Scene loadingScreenScene = SceneManager.GetSceneByName(Constants.SCENE_NAME_LOADINGSCREEN);
+		AsyncOperation asyncUnload = SceneManager.UnloadSceneAsync(loadingScreenScene);
+
+		while (!asyncUnload.isDone)
+		{
+			yield return null;
+		}
+
+		// Once unloading is done, can destroy itself.
+		GameObject.Destroy(this.gameObject);
+	}
+}
