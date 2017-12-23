6541aa7bde9f17dfed1fa76040641c005f7914e6
raynertanxw <raynertanxw@me.com>
2017-12-23 23:09:08 +0800

    Added lerping

--- Chessgeon/Assets/_Game/Scripts/Card.cs
+++ Chessgeon/Assets/_Game/Scripts/Card.cs
@@ -7,37 +7,61 @@ public class Card : MonoBehaviour
 {
 	// TODO: Card types and attributes.
 
-	private Vector2 _originAnchorPos;
+	private Vector3 _originLocalPos;
 	private RectTransform _cardRectTransform = null;
 	private float _originZ;
 
+	private Vector3 _desiredCardLocalPos;
+	private float _lerpSpeed;
+
 	private const float HOLDING_CARD_Z_OFFSET = 1.0f;
+	private const float DRAGGING_LERP_SPEED = 20.0f;
+	private const float SNAPPING_BACK_LERP_SPEED = 30.0f;
 
 	private void Awake()
 	{
 		_cardRectTransform = gameObject.GetComponent<RectTransform>();
-		_originAnchorPos = _cardRectTransform.anchoredPosition;
-		_originZ = _cardRectTransform.position.z;
+	}
+
+	private void Start()
+	{
+		_desiredCardLocalPos = _cardRectTransform.localPosition;
+		_originLocalPos = _cardRectTransform.localPosition;
+		_originZ = _cardRectTransform.localPosition.z;
+	}
+
+	private void Update()
+	{
+		_cardRectTransform.localPosition = Vector3.Lerp(_cardRectTransform.localPosition, _desiredCardLocalPos, Time.deltaTime * _lerpSpeed);
 	}
 
 	public void EventTriggerOnPointerDown(BaseEventData data)
 	{
-		_cardRectTransform.position += Vector3.forward * -HOLDING_CARD_Z_OFFSET;
+		_cardRectTransform.localPosition += Vector3.forward * -HOLDING_CARD_Z_OFFSET;
+
+		_desiredCardLocalPos = _cardRectTransform.localPosition;
+		_lerpSpeed = DRAGGING_LERP_SPEED;
 	}
 
 	public void EventTriggerOnPointerUp(BaseEventData data)
 	{
-		_cardRectTransform.anchoredPosition = _originAnchorPos;
-		Vector3 originZ = _cardRectTransform.position;
+		Vector3 originZ = _cardRectTransform.localPosition;
 		originZ.z = _originZ;
-		_cardRectTransform.position = originZ;
+		_cardRectTransform.localPosition = originZ;
+
+		_desiredCardLocalPos = _originLocalPos;
+		_lerpSpeed = SNAPPING_BACK_LERP_SPEED;
 	}
 
 	public void EventTriggerOnDrag(BaseEventData data)
 	{
 		PointerEventData ptrEventData = (PointerEventData)data;
 		Vector3 desiredPos = UICamera.Camera.ScreenToWorldPoint(ptrEventData.position);
-		desiredPos.z = _cardRectTransform.position.z;
-		_cardRectTransform.position = desiredPos;
+
+		// TODO: Figure out why this code needs such arbitrary values. Would be best if the InverseTransformPoint worked.
+		Vector3 localVerOfDesiredPos = UICamera.Camera.transform.InverseTransformPoint(desiredPos);
+		localVerOfDesiredPos.z = -HOLDING_CARD_Z_OFFSET;
+		_desiredCardLocalPos = localVerOfDesiredPos + new Vector3(0.68f, 5.0f);
+		//Debug.Log(desiredPos + " : " + localVerOfDesiredPos);
 	}
 }
