c7d8bb338b9372f9b52f394a600fd2d5c8b7988b
Tan Xue Wen, Rayner <raynertanxw@me.com>
2017-12-25 10:52:53 +0800

    Implemented Card dynamic tilting

--- Chessgeon/Assets/_Game/Scripts/Card.cs
+++ Chessgeon/Assets/_Game/Scripts/Card.cs
@@ -13,10 +13,15 @@ public class Card : MonoBehaviour
 
 	private Vector3 _desiredCardLocalPos;
 	private float _lerpSpeed;
+	private Vector2 _prevFrameLocalPos;
+	private Vector2 _tiltIntertia;
 
 	private const float HOLDING_CARD_Z_OFFSET = 1.0f;
 	private const float DRAGGING_LERP_SPEED = 20.0f;
 	private const float SNAPPING_BACK_LERP_SPEED = 30.0f;
+	private readonly Vector2 TILT_INTERTIA_FACTOR = new Vector2(25.0f, 12.5f);
+	private const float TILT_INTERTIA_DRAG = 5.0f; 
+	private const float MAX_TILT = 35.0f;
 
 	private void Awake()
 	{
@@ -33,6 +38,8 @@ public class Card : MonoBehaviour
 	private void Update()
 	{
 		_cardRectTransform.localPosition = Vector3.Lerp(_cardRectTransform.localPosition, _desiredCardLocalPos, Time.deltaTime * _lerpSpeed);
+		_cardRectTransform.localRotation = Quaternion.Euler(_tiltIntertia.y, _tiltIntertia.x, 0.0f);
+		_tiltIntertia = Vector2.Lerp(_tiltIntertia, Vector2.zero, Time.deltaTime * TILT_INTERTIA_DRAG);
 	}
 
 	public void EventTriggerOnPointerDown(BaseEventData data)
@@ -40,6 +47,8 @@ public class Card : MonoBehaviour
 		_cardRectTransform.localPosition += Vector3.forward * -HOLDING_CARD_Z_OFFSET;
 
 		_desiredCardLocalPos = _cardRectTransform.localPosition;
+		_tiltIntertia = Vector2.zero;
+		_prevFrameLocalPos = _cardRectTransform.localPosition;
 		_lerpSpeed = DRAGGING_LERP_SPEED;
 	}
 
@@ -63,5 +72,11 @@ public class Card : MonoBehaviour
 		localVerOfDesiredPos.z = -HOLDING_CARD_Z_OFFSET;
 		_desiredCardLocalPos = localVerOfDesiredPos + new Vector3(0.68f, 5.0f);
 		//Debug.Log(desiredPos + " : " + localVerOfDesiredPos);
+
+		Vector2 tiltIntertiaDelta = _prevFrameLocalPos - (Vector2)_cardRectTransform.localPosition;
+		tiltIntertiaDelta.Scale(TILT_INTERTIA_FACTOR);
+		_tiltIntertia += tiltIntertiaDelta;
+		_tiltIntertia = Vector2.ClampMagnitude(_tiltIntertia, MAX_TILT);
+		_prevFrameLocalPos = _cardRectTransform.localPosition;
 	}
 }
