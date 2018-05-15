08bb25ce2c8dfe190df956572dfa424bb035bd5e
raynertanxw <raynertanxw@me.com>
2018-05-15 09:21:35 +0800

    Append to last commit.

--- Chessgeon/Assets/_Game/Scripts/Editor/EditorMenuItems.cs
+++ Chessgeon/Assets/_Game/Scripts/Editor/EditorMenuItems.cs
@@ -6,19 +6,26 @@ using DaburuTools;
 
 public class EditorMenuItems
 {
-    private const string MENU_ITEM_PREFIX = "Chessgeon/";
-    public const string SCENE_PATH_LOADING_SCREEN = "Assets/_Game/Scenes/SCENE_LoadingScreen.unity";
-    public const string SCENE_PATH_DUNGEON = "Assets/_Game/Scenes/SCENE_Dungeon.unity";
+	private const string MENU_ITEM_PREFIX = "Chessgeon/";
+	public const string SCENE_PATH_LOADING_SCREEN = "Assets/_Game/Scenes/SCENE_LoadingScreen.unity";
+	public const string SCENE_PATH_DUNGEON = "Assets/_Game/Scenes/SCENE_Dungeon.unity";
 
-    [MenuItem(MENU_ITEM_PREFIX + "Open Loading Screen Scene", false, 1)]
-    private static void OpenLoadingScreenScene() { DTEditorUtility.OpenScene(SCENE_PATH_LOADING_SCREEN); }
+	[MenuItem(MENU_ITEM_PREFIX + "Open Loading Screen Scene", false, 1)]
+	private static void OpenLoadingScreenScene() { DTEditorUtility.OpenScene(SCENE_PATH_LOADING_SCREEN); }
 
-    [MenuItem(MENU_ITEM_PREFIX + "Open Dungeon Scene", false, 2)]
-    private static void OpenDungeonScene() { DTEditorUtility.OpenScene(SCENE_PATH_DUNGEON); }
+	[MenuItem(MENU_ITEM_PREFIX + "Open Dungeon Scene", false, 2)]
+	private static void OpenDungeonScene() { DTEditorUtility.OpenScene(SCENE_PATH_DUNGEON); }
 
-    [MenuItem(MENU_ITEM_PREFIX + "Build Chessgeon DEBUG")]
+#if UNITY_ANDROID
+	[MenuItem(MENU_ITEM_PREFIX + "Build Chessgeon DEBUG")]
     private static void BuildChessgeonDebug() { ChessgeonBuildScript.BuildChessgeon(eBuildScheme.DEBUG); }
+#elif UNITY_IOS
+	[MenuItem(MENU_ITEM_PREFIX + "Build Chessgeon DEBUG (Append)")]
+    private static void BuildChessgeonDebugAppend() { ChessgeonBuildScript.BuildChessgeon(eBuildScheme.DEBUG, true); }
+	[MenuItem(MENU_ITEM_PREFIX + "Build Chessgeon DEBUG (Replace)")]
+    private static void BuildChessgeonDebugReplace() { ChessgeonBuildScript.BuildChessgeon(eBuildScheme.DEBUG); }
+#endif
 
-    [MenuItem(MENU_ITEM_PREFIX + "Build Chessgeon RELEASE")]
+	[MenuItem(MENU_ITEM_PREFIX + "Build Chessgeon RELEASE")]
     private static void BuildChessgeonRelease() { ChessgeonBuildScript.BuildChessgeon(eBuildScheme.RELEASE); }
 }
