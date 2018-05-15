033cf9a52d6756201288451736cf5746d4153a90
raynertanxw <raynertanxw@me.com>
2018-05-15 08:48:24 +0800

    Added Mock iPhone X tool

---
+++ Chessgeon/Assets/DaburuTools/Utility/Testing/Editor/MockiPhoneXBorderEditorMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MockiPhoneXBorderEditorMenu
+{
+	[MenuItem("DaburuTools/Utility/Add Mock iPhoneX Border", false, 1)]
+	private static void DT_Util_AddMockiPhoneXBorder()
+	{
+		Object MockiPhoneXBoderPrefab = AssetDatabase.LoadAssetAtPath("Assets/DaburuTools/Utility/Testing/Mock iPhoneX Borders.prefab", typeof(GameObject));
+		GameObject mockiPhoneXBorder = PrefabUtility.InstantiatePrefab(MockiPhoneXBoderPrefab) as GameObject;
+		Undo.RegisterCreatedObjectUndo(mockiPhoneXBorder, "Create Mock iPhoneX Border");
+		// Remove the "(Clone)" of the name.
+		//mockiPhoneXBorder.name = mockiPhoneXBorder.name.Remove(mockiPhoneXBorder.name.Length - 7);
+		Debug.Log("Mock iPhoneX Border created successfully.");
+	}
+}
