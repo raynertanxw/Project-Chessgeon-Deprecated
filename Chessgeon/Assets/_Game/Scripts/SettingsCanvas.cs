5474750adb39cd60549dc18b26caf505609e903f
raynertanxw <raynertanxw@me.com>
2018-06-07 00:04:28 +0800

    Added settings canvas which has a reset tutorial button.

---
+++ Chessgeon/Assets/_Game/Scripts/SettingsCanvas.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsCanvas : MonoBehaviour
+{
+	[SerializeField] private Button _settingsBtn = null;
+
+	[Header("Settings Canvas")]
+	[SerializeField] private Canvas _settingCanvas = null;
+	[SerializeField] private Button _closeBtn = null;
+	[SerializeField] private Button _resetTutorialBtn = null;
+
+	private void Awake()
+	{
+		Debug.Assert(_settingsBtn != null, "_settingBtn is not assigned.");
+
+		Debug.Assert(_settingCanvas != null, "_settingCanvas is not assigned.");
+		Debug.Assert(_closeBtn != null, "_closeBtn is not assigned.");
+		Debug.Assert(_resetTutorialBtn != null, "_resetTutorialBtn is not assigned.");
+
+		_settingsBtn.onClick.AddListener(() => { SetSettingCanvasVisible(true); });
+
+		_closeBtn.onClick.AddListener(() => { SetSettingCanvasVisible(false); });
+		_resetTutorialBtn.onClick.AddListener(ResetTutorial);
+
+		SetSettingCanvasVisible(false);
+	}
+
+	private void SetSettingCanvasVisible(bool inIsVisible)
+	{
+		_settingCanvas.enabled = inIsVisible;
+	}
+
+	private void ResetTutorial()
+	{
+		// TODO: Implement this.
+		Debug.Log("Reset Tutorial");
+	}
+}
